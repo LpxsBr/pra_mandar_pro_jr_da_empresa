37b557621458f996a2b01cbdbff0219337a69a63
lpxsbr <anselmolopes.an@gmail.com>
2026-05-21 08:06:59 -0300

    first commit

---
+++ matheus_busca_valor_array.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * A idéia é que retorne o valor dentro da cadeia de arrays, independente do número de níveis que ela tenha.
+ * O valor a ser retornado é 'valor para retornar'.
+ */
+
+public function buscarValorNoArray($array){
+
+}
+
+echo buscarValorNoArray([[[[[[[[[[[[[[[['valor para retornar']]]]]]]]]]]]]]]]);
+
+?>
